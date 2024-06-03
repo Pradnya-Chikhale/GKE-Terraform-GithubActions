83816d03f427382b170d034a860d28f73f4c4b6e
Pradnya Chikhale <pradnyach27@gmail.com>
2024-06-03 11:39:41 +0530

    updated application code

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('stable', views.index, name="index_stable"),
+    path('canary', views.index, name="index_canary"),
     
 ]
--- crudapp/views.py
+++ crudapp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import Student
 from django.contrib import messages
 from django.db.models import Q
-from django.http import HttpResponse
-
 
 def index(request):
     students = Student.objects.all()
     search_query = ""
-    if request.method == "POST": 
+    path = request.path
+    
+    if request.method == "POST":
         if "create" in request.POST:
             name = request.POST.get("name")
             email = request.POST.get("email")
@@ -26,17 +26,16 @@ def index(request):
             student.name = name
             student.email = email
             student.save()
-            messages.success(request, "student updated successfully")
+            messages.success(request, "Student updated successfully")
     
         elif "delete" in request.POST:
             id = request.POST.get("id")
             Student.objects.get(id=id).delete()
-            messages.success(request, "student deleted successfully")
+            messages.success(request, "Student deleted successfully")
         
         elif "search" in request.POST:
             search_query = request.POST.get("query")
             students = Student.objects.filter(Q(name__icontains=search_query) | Q(email__icontains=search_query))
 
-    context = {"students": students, "search_query": search_query}
+    context = {"students": students, "search_query": search_query, "path": path}
     return render(request, "index.html", context=context)
-
